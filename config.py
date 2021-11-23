7d37c2cc3b02be7a190bac790e666472e464293a
serviux <jcorm714@gmail.com>
2021-11-23 04:24:42 -0500

    Added drafting

---
+++ config.py
@@ -0,0 +1,16 @@
+class Config: 
+        # the following filter which classes of pokemon to use
+        # available values:
+        #       "legendary", 
+        #       "mythical",
+        #       "sub_legendary",
+        #       "ultra_beast"
+        DONT_USE = ["legendary", "mythical","sub_legendary","ultra_beast"]
+
+        # how big the ending pool will be 
+        SAMPLE_SIZE = 50
+
+
+        # file data gets saved too
+        OUT_FILE = "out.csv"
+
